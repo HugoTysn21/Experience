2f08214ce2cc624e2631595c3a924d5211ac4562
Hugo <hugola1@hotmail.fr>
2019-03-10 15:38:13 +0100

    modif miogrations

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,6 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
             })
     }
 
+    if (document.body.classList.contains('about')) {
+        new Typed('#typed',
+            {
+                typeSpeed: 45,
+                backSpeed: 5,
+                backDelay: 150,
+                strings: ["return  URLSession\n .shared\n .rx\n .response(method: .get, u\n","Hello","I am Hugo Lantillon, I am 19 years old, I live and work in Lyon.\n" +
+                "I started at Ynov Campus University after a Technical Bachelor's degree in Computer Science,\n" +
+                "I am currently in the 2nd year of my Bachelor's degree where I specialized in programming,\n" +
+                "I quickly developed this passion following a project that was carried out both at the university and outside."]
+            });
+    }
+
     // if (document.body.classList.contains('front-page')) {
     //     new Typed('#js_typed_title span', {
     //         loop: true,
