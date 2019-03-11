c83e6f149d1ccabf5b809e67b980a9872094be22
Hugo <hugola1@hotmail.fr>
2019-03-11 10:51:04 +0100

    update background home

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,19 +12,19 @@ class HomepageController extends AbstractController
      */
     public function index()
     {
-//        $ramdomImg = [
-//            '/uploads/asia.jpg',
-//            '/uploads/pexels-photo-940035.jpeg',
-//            '/uploads/austin-neill-189146-unsplash.jpg',
-//            '/uploads/pexels-photo-1112109.jpeg',
-//            '/uploads/pexels-photo-256219.jpeg',
-//        ];
-//
-//        $randomIndex = rand(0, sizeof($ramdomImg)-1);
+        $ramdomImg = [
+            '/uploads/asia.jpg',
+            '/uploads/pexels-photo-940035.jpeg',
+            '/uploads/austin-neill-189146-unsplash.jpg',
+            '/uploads/pexels-photo-1112109.jpeg',
+            '/uploads/pexels-photo-256219.jpeg',
+        ];
+
+        $randomIndex = rand(0, sizeof($ramdomImg)-1);
 
         return $this->render('homepage/index.html.twig', [
             'controller_name'   => 'HomepageController',
-//            'background'        => $ramdomImg[$randomIndex]
+            'background'        => $ramdomImg[$randomIndex]
         ]);
     }
 }
