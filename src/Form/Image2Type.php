129f22419e985241b86d880d9acf2a77514dea8e
Hugo <hugola1@hotmail.fr>
2019-03-11 18:36:42 +0100

    update for upload correct

--- src/Form/Image2Type.php
+++ src/Form/Image2Type.php
@@ -4,15 +4,21 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class Image2Type extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('content')
+            ->add('avatar',FileType::class)
             ->add('title')
         ;
     }
--- src/Form/Post5Type.php
+++ src/Form/Post5Type.php
@@ -4,11 +4,16 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class Post5Type extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -18,6 +23,7 @@ class Post5Type extends AbstractType
             ->add('createdAt')
             ->add('introductionPost')
             ->add('avatarDirectory')
+            ->add('avatar',FileType::class)
             ->add('content')
         ;
     }
